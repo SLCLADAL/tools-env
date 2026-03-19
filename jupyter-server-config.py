93efd545e61185a459faae12488b15107a226f27
Martin Schweinberger <44954005+MartinSchweinberger@users.noreply.github.com>
2026-03-19 11:25:30 +1100

    Create jupyter-server-config.py

---
+++ jupyter-server-config.py
@@ -0,0 +1,10 @@
+c.ServerProxy.servers = {
+    "wordfinder": {
+        "command": ["R", "-e", "shiny::runApp('/home/jovyan/tools/wordfinder', port={port}, host='0.0.0.0')"],
+        "port": 3838,
+        "timeout": 30,
+        "launcher_entry": {
+            "enabled": False
+        }
+    }
+}
